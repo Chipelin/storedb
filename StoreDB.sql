382111e31e632a5d25da4b1362de27438e4d1628
Chipelin <mrchipelin@gmail.com>
2022-07-22 14:24:27 +0800

    Requested changes
    Added: Product_Category and Product_Image tables. Deleted :  Brand table. Changed:  prices to float. New diagram

--- StoreDB.sql
+++ StoreDB.sql
@@ -32,45 +32,55 @@ CREATE TABLE IF NOT EXISTS `Storedb`.`Brand` (
 
 CREATE TABLE IF NOT EXISTS `Storedb`.`Product` (
   `id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `category_id` INT NULL,
-  `brand_id` INT NULL,
-  `count` INT NULL,
-  `baseprice` INT NULL,
-  `discountprice` INT NULL,
-  `currentprice` VARCHAR(45) NULL,
-  `image_id` INT NULL,
-  `description` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`),
-  INDEX `category_id_idx` (`category_id` ASC),
-  INDEX `brand_id_idx` (`brand_id` ASC),
-  INDEX `image_id_idx` (`image_id` ASC),
-  CONSTRAINT `category_id`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `Storedb`.`Category` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `count` INT NULL DEFAULT NULL,
+  `baseprice` FLOAT NULL DEFAULT NULL,
+  `discountprice` FLOAT NULL DEFAULT NULL,
+  `currentprice` FLOAT NULL DEFAULT NULL,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE IF NOT EXISTS `Storedb`.`Product_Image` (
+  `image_id` INT NOT NULL,
+  `product_id` INT NOT NULL,
+  PRIMARY KEY (`image_id`, `product_id`),
+  INDEX `product_id_idx` (`product_id` ASC),
   CONSTRAINT `image_id`
     FOREIGN KEY (`image_id`)
     REFERENCES `Storedb`.`Images` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `brand_id`
-    FOREIGN KEY (`brand_id`)
-    REFERENCES `Storedb`.`Brand` (`id`)
+  CONSTRAINT `product_id`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `Storedb`.`Product` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE IF NOT EXISTS `Storedb`.`Product_Category` (
+  `product_id` INT NOT NULL,
+  `category_id` INT NOT NULL,
+  PRIMARY KEY (`product_id`, `category_id`),
+  INDEX `category_id_idx` (`category_id` ASC),
+  CONSTRAINT `product_id`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `Storedb`.`Product` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `category_id`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `Storedb`.`Category` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
-INSERT INTO `Storedb`.`Brand` (`id`, `name`, `country`, `description`) VALUES (1, 'brand1', 'somecountry', 'brand 1 description');
-INSERT INTO `Storedb`.`Brand` (`id`, `name`, `country`, `description`) VALUES (2, 'brand2', 'counry2', 'brand 2 desc');
+
 INSERT INTO `Storedb`.`Category` (`id`, `name`, `description`) VALUES (1, 'cat1', 'cat1 description');
 INSERT INTO `Storedb`.`Category` (`id`, `name`, `description`) VALUES (2, 'cat2', 'cat2 description');
 INSERT INTO `Storedb`.`Category` (`id`, `name`, `description`) VALUES (3, 'cat3', 'cat3 description');
 INSERT INTO `Storedb`.`Images` (`id`, `name`, `path`, `alt`) VALUES (1, 'img1', 'path\\to\\img', 'alt1');
 INSERT INTO `Storedb`.`Images` (`id`, `name`, `path`, `alt`) VALUES (2, 'img2', 'path\\to\\img2', 'alt2');
 INSERT INTO `Storedb`.`Images` (`id`, `name`, `path`, `alt`) VALUES (3, 'img3', 'path\\to\\img3', 'alt3');
-INSERT INTO `Storedb`.`Product` (`id`, `name`, `category_id`, `brand_id`, `count`, `baseprice`, `discountprice`, `currentprice`, `image_id`, `description`) VALUES (1, 'product1', 1, 1, 56, 56, 50, '50', 1, 'desc');
+INSERT INTO `Storedb`.`Product` (`id`, `name`, `count`, `baseprice`, `discountprice`, `currentprice`, `description`) VALUES (1, 'product1', 10, 1, 56, 56,  'desc');
 
 
 
